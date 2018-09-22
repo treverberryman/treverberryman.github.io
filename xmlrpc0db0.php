9be839ac08b7786619e429cf679139a874dc0a23
Trever Berryman <tberry5@lsu.edu>
2018-09-22 00:48:50 -0700

    Link preview testing

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -1,13 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
   <service>
     <engineName>WordPress</engineName>
-    <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>https://trbweb.wordpress.com</homePageLink>
+    <engineLink>https://treverberryman.github.io/</engineLink>
+    <homePageLink>https://treverberryman.github.io/</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="https://trbweb.wordpress.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://trbweb.wordpress.com/xmlrpc.php" />
       <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://trbweb.wordpress.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="https://trbweb.wordpress.com/xmlrpc.php" />
       <api name="Twitter" blogID="" preferred="false" apiLink="https://twitter-api.wordpress.com/" />
     </apis>
   </service>
